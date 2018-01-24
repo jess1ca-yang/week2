4df3c45cb7eeb07cdbc4310014056f2dafc91e9a
Jessica Yang <jessica.yang@cca.edu>
2018-01-24 10:54:01 -0800

    Update Puzzle9 - iii.js

--- Puzzle9 - iii.js
+++ Puzzle9 - iii.js
@@ -10,38 +10,23 @@ function draw(direction, n) {
   var count = 0
 	while (count < n) {
   setColor("blue");
-  right();
+  direction();
   count = count + 1
 	}
 }
+
+
+repeat(right, 14)
 
 function rect(x, y, width, height) {
   var count = 0
   repeat(right, x);
   repeat(down, y);
-  while (count < width){
-  setColor("blue");
-  right();
-  count = count + 1
-}
-  while (count < height){
-  setColor("blue");
-  down();
-	count = count + 1
-  }
-  count = 0
-  while (count < width){
-  setColor("blue");
-  left();
-	count = count + 1
-  }
-  count = 2
-  while (count < height){
-  setColor("blue");
-  up();
-	count = count + 1
-  }
+  draw(right, width);
+	draw(down, height);
+	draw(left, width);
+	draw(up, height);
 }
 
 
-rect(2, 3, 2, 5);
+rect(2, 3, 6, 7);
